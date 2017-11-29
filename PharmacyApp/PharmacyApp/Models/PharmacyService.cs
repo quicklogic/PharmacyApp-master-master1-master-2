93a1e7e3793a0c1f88c873560891fe236582f89c
temp1 <temp1@TEMP1-PC>
2017-11-29 16:46:36 +0300

    Add project files.

---
+++ PharmacyApp/PharmacyApp/Models/PharmacyService.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using PharmacyApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyApp.Models
+{
+    public class PharmacyService
+    {
+        public object Navigation { get; private set; }
+
+        private HttpClient GetClient()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            return client;
+        }
+
+        public async Task<IEnumerable<Product>> Get()
+        {
+
+           HttpClient client = GetClient();
+           string result = await client.GetStringAsync("http://pharmacyapiprototype123.azurewebsites.net/api/products");
+           return JsonConvert.DeserializeObject<IEnumerable<Product>>(result);
+           
+        }
+
+        public async Task<UserInfo> Add(UserInfo user)
+        {
+            HttpClient client = GetClient();
+            var response = await client.PostAsync("http://pharmacyapiprototype123.azurewebsites.net/api/user",
+                new StringContent(
+                    JsonConvert.SerializeObject(user),
+                    Encoding.UTF8, "application/json"));
+
+            if (response.StatusCode != HttpStatusCode.OK)
+                return null;
+            return user;
+        }
+
+
+        public async Task<User> Login(User user)
+        {
+            HttpClient client = GetClient();
+            var response = await client.PostAsync("http://pharmacyapiprototype123.azurewebsites.net/api/login",
+                new StringContent(
+                    JsonConvert.SerializeObject(user),
+                    Encoding.UTF8, "application/json"));
+
+            if (response.StatusCode != HttpStatusCode.OK)
+                return null;
+            else return user;
+        }
+
+    }
+}
---
+++ PharmacyApp/PharmacyApp/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,76 @@
+﻿using PharmacyApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PharmacyApp.ViewModels
+{
+    public class LoginPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand EnterCommand { get; set; }
+        INavigation Navigation { get; set; }
+
+        private string login;
+        private string password;
+
+        protected void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public string Login
+        {
+            get { return login; }
+            set
+            {
+                if (!String.IsNullOrWhiteSpace(value))
+                {
+                    login = value;
+                    OnPropertyChanged("Login");
+                }
+            }
+        }
+
+        public string Password
+        {
+
+            get { return password; }
+            set
+            {
+                if (!String.IsNullOrWhiteSpace(value))
+                {
+                    password = value;
+                    OnPropertyChanged("Password");
+                }
+            }
+        }
+        public LoginPageViewModel()
+        {
+            EnterCommand = new Command(Enter);
+        }
+
+        private async void Enter()
+        {
+            if (!String.IsNullOrWhiteSpace(Login) && !String.IsNullOrWhiteSpace(Password))
+            {
+                User user = new User
+                {
+                    login = Login,
+                    password = Password
+                };
+
+                PharmacyService service = new PharmacyService();
+
+               await service.Login(user);
+
+            }
+        }
+    }
+}
